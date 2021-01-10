1efe0386b113550d755067b12048fec0c5f9ae4a
Niklas Tegnander <niklas@tegnander.nu>
2021-01-10 15:28:47 +0100

    Working react-pixi-fiber

--- assets/src/index.tsx
+++ assets/src/index.tsx
@@ -1,15 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import * as PIXI from "pixi.js";
+import { render } from "react-dom";
+import { Sprite, Stage } from "react-pixi-fiber";
+import bunny from "./bunny.png";
 import { Socket } from "phoenix";
 
 let socket = new Socket("/socket", { params: { userToken: "123" } });
 socket.connect();
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+function Bunny(props: any) {
+  return <Sprite texture={PIXI.Texture.from(bunny)} {...props} />;
+}
+
+render(
+  <Stage options={{ backgroundColor: 0x10bb99, height: 600, width: 800 }}>
+    <Bunny x={200} y={200} />
+  </Stage>,
   document.getElementById("root")
 );
